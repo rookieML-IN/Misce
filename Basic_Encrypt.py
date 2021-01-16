4211aa1cddde29d2de4a5aba18757b510e85ff71
Ashutosh Das <33962783+rookieML-IN@users.noreply.github.com>
2021-01-17 01:07:45 +0530

    Add files via upload

---
+++ Basic_Encrypt.py
@@ -0,0 +1,14 @@
+def encrypt():
+    message = input("Please Enter the string :")
+    for i in message:
+        def factorial(message):
+            fact = 1
+            for j in range(len(message)):
+                f1 = (fact*j)+ord(i)
+                return  f1
+        print(ord(i) + factorial(message), end=" ")
+
+
+
+
+encrypt()
